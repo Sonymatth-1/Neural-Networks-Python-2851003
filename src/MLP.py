e858f91a780c0515cbf4ca707d712382eb75b52f
Sonymatth-1 <sonibaremo@yahoo.co.uk>
2022-11-11 08:47:12 +0000

    Update MLP.py

--- src/MLP.py
+++ src/MLP.py
@@ -20,6 +20,18 @@ class Perceptron:
 
     def set_weights(self, w_init):
         # w_init is a list of floats. Organize it as you'd like.
+        self.weights = np.array(w_init)
 
     def sigmoid(self, x):
         # return the output of the sigmoid function applied to x
+        return 1/(1+np.exp(-x))
+
+#test code
+neuron = Perceptron(inputs=2)
+neuron.set_weights([10,10,-15]) #AND
+
+print("Gate")
+print("0 0 = {0:.10f}".format(neuron.run([0,0])))
+print("0 1 = {0:.10f}".format(neuron.run([0,0])))
+print("1 0 = {0:.10f}".format(neuron.run([0,0])))
+print("1 1 = {0:.10f}".format(neuron.run([0,0])))
